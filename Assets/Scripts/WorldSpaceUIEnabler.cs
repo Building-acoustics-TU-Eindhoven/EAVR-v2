40624c1dec0ba8e9ac0c279ed133dd7c5bfd4b2d
SilvinWillemsen <silvinwillemsen@gmail.com>
2024-03-20 18:11:16 +0100

    Added comment to previous commit

--- Assets/Scripts/WorldSpaceUIEnabler.cs
+++ Assets/Scripts/WorldSpaceUIEnabler.cs
@@ -1,3 +1,8 @@
+// This script allows for a locked cursor to be able to interact with World Space UI elements
+
+// Taken from https://forum.unity.com/threads/world-space-canvas-cursorlockmode-locked-incompatible.566485/
+// Also, needed to go to Project Settings -> Script Execution Order -> Put this script at the top
+
 using UnityEngine;
 
 class WorldSpaceUIEnabler : MonoBehaviour
