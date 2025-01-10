5dce1139376df65927893fff821c3616d681402f
SilvinWillemsen <silvinwillemsen@gmail.com>
2025-01-10 10:46:53 +0100

    Player not automatically centered.

--- Assets/Scripts/MenuFunctions/RoomMenuManager.cs
+++ Assets/Scripts/MenuFunctions/RoomMenuManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Inherits from the interface in PlayerManager to allow for bidirectional communication between the classes
-public class RoomMenuManager : SubMenu, iRoomMenuManager 
+public class RoomMenuManager : SubMenu, iRoomMenuManager
 {
     // The GameObject holding the room geometry 
     public GameObject root;
@@ -15,10 +15,10 @@ public class RoomMenuManager : SubMenu, iRoomMenuManager
     public Vector3 curRoomSize;
 
     // Player position normalised between -0.5 and 0.5 for X and Z and between 0 and 1 for Y.
-    private Vector3 normalisedPlayerPos = new Vector3 (0.5f, 0.2f, 0.5f);
+    private Vector3 normalisedPlayerPos = new Vector3(0.5f, 0.2f, 0.5f);
 
     // Non normalised player position (for use in PlayerManager Update() to check if this class has the latest player position
-    private Vector3 nonNormalisedPlayerPos = new Vector3 (0.0f, 1.0f, 0.0f);
+    private Vector3 nonNormalisedPlayerPos = new Vector3(0.0f, 1.0f, 0.0f);
 
     // The player
     public GameObject playerGO;
@@ -35,8 +35,6 @@ public class RoomMenuManager : SubMenu, iRoomMenuManager
 
     private float playerColliderRadius = 0.5f;
 
-    private bool prepared = false;
-
     [SerializeField]
     private KnobButton xKnob, yKnob, zKnob;
 
@@ -52,16 +50,17 @@ public class RoomMenuManager : SubMenu, iRoomMenuManager
         originalSize = b.size;
         curRoomSize = originalSize;
 
-        audioSourceManager.SetOriginalRoomDimensions (originalSize);
+        audioSourceManager.SetOriginalRoomDimensions(originalSize);
 
         playerActive = playerGO.activeSelf;
         if (playerActive)
         {
             playerManager = playerGO.GetComponent<PlayerManager>();
             RefreshInternalPlayerPos(false);
-            playerManager.PreparePlayerPosAndRoomDimensions (normalisedPlayerPos, originalSize);
+            playerManager.PreparePlayerPosAndRoomDimensions(normalisedPlayerPos, originalSize);
         }
-        else {
+        else
+        {
             xrOriginManager = xrOrigin.GetComponent<XRoriginManager>();
             RefreshInternalPlayerPos(true);
 
@@ -69,13 +68,12 @@ public class RoomMenuManager : SubMenu, iRoomMenuManager
 
         RefreshTextAndUIElements();
 
-        prepared = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public Vector3 GetNormalisedPlayerPos()
@@ -84,20 +82,18 @@ public class RoomMenuManager : SubMenu, iRoomMenuManager
     }
 
     // Doesn't change the player position, simply retrieves its position as a ratio of the room width and height
-    public void RefreshInternalPlayerPos (bool isXRorigin)
+    public void RefreshInternalPlayerPos(bool isXRorigin)
     {
-        if (!prepared)
-            return;
         nonNormalisedPlayerPos = playerActive ? playerGO.transform.position : xrOrigin.transform.position;
 
-        normalisedPlayerPos = new Vector3 ((nonNormalisedPlayerPos.x - root.transform.localPosition.x) / (curRoomSize.x - playerColliderRadius) + 0.5f, // Subtract radius so that the normalised player position all the way at the edge is 0 or 1
+        normalisedPlayerPos = new Vector3((nonNormalisedPlayerPos.x - root.transform.localPosition.x) / (curRoomSize.x - playerColliderRadius) + 0.5f, // Subtract radius so that the normalised player position all the way at the edge is 0 or 1
                                            ((nonNormalisedPlayerPos.y - (isXRorigin ? 0.0f : 1.0f)) - root.transform.localPosition.y) / curRoomSize.y,                // Subtract 1.0f as this is half the height of the player collider
                                            (nonNormalisedPlayerPos.z - root.transform.localPosition.z) / (curRoomSize.z - playerColliderRadius) + 0.5f); // Subtract radius so that the normalised player position all the way at the edge is 0 or 1
 
         //Debug.Log("Normalised pos: " + normalisedPlayerPos);
     }
 
-    public void SetRoomXfromKnob (KnobButton knob)
+    public void SetRoomXfromKnob(KnobButton knob)
     {
         // Obtain value from the knob
         float xScaling = knob.GetValue();
@@ -106,10 +102,10 @@ public class RoomMenuManager : SubMenu, iRoomMenuManager
         root.transform.localScale = new Vector3(xScaling, root.transform.localScale.y, root.transform.localScale.z);
 
         // Set the internal room width variable by scaling the original width 
-        curRoomSize = new Vector3 (xScaling * originalSize.x, curRoomSize.y, curRoomSize.z);
-        
+        curRoomSize = new Vector3(xScaling * originalSize.x, curRoomSize.y, curRoomSize.z);
+
         // Update the source location
-        audioSourceManager.SetSourcesFromRoomSizeX (curRoomSize.x);
+        audioSourceManager.SetSourcesFromRoomSizeX(curRoomSize.x);
 
         // Update the player X location
         if (playerActive)
@@ -119,19 +115,19 @@ public class RoomMenuManager : SubMenu, iRoomMenuManager
 
     }
 
-    public void SetRoomYfromKnob (KnobButton knob)
+    public void SetRoomYfromKnob(KnobButton knob)
     {
         // Obtain value from the knob
         float yScaling = knob.GetValue();
-                
+
         // Set the height of the room geometry
         root.transform.localScale = new Vector3(root.transform.localScale.x, yScaling, root.transform.localScale.z);
 
         // Set the internal room width variable by scaling the original width 
-        curRoomSize = new Vector3 (curRoomSize.x, yScaling * originalSize.y, curRoomSize.z);
+        curRoomSize = new Vector3(curRoomSize.x, yScaling * originalSize.y, curRoomSize.z);
 
         // Update the source location
-        audioSourceManager.SetSourcesFromRoomSizeY (curRoomSize.y);
+        audioSourceManager.SetSourcesFromRoomSizeY(curRoomSize.y);
 
         // Update the player Y location
         if (playerActive)
@@ -141,20 +137,20 @@ public class RoomMenuManager : SubMenu, iRoomMenuManager
 
     }
 
-    public void SetRoomZfromKnob (KnobButton knob)
+    public void SetRoomZfromKnob(KnobButton knob)
     {
         // Obtain value from the knob
         float zScaling = knob.GetValue();
-        
+
         // Set the depth of the room geometry
         root.transform.localScale = new Vector3(root.transform.localScale.x, root.transform.localScale.y, zScaling);
 
         // Set the internal room width variable by scaling the original width 
-        curRoomSize = new Vector3 (curRoomSize.x, curRoomSize.y, zScaling * originalSize.z);
+        curRoomSize = new Vector3(curRoomSize.x, curRoomSize.y, zScaling * originalSize.z);
 
         // Update the source location
-        audioSourceManager.SetSourcesFromRoomSizeZ (curRoomSize.z);
-       
+        audioSourceManager.SetSourcesFromRoomSizeZ(curRoomSize.z);
+
         // Update the player Z location
         if (playerActive)
             playerManager.SetPlayerZ(normalisedPlayerPos.z, curRoomSize.z);
@@ -178,37 +174,43 @@ public class RoomMenuManager : SubMenu, iRoomMenuManager
     private void SetKnobValuesFromCurrentRoomDimensions()
     {
         // As the scaling is not normalised (not between 0 and 1) use the SetNonNormalisedValue() function
-        xKnob.SetNonNormalisedValue (curRoomSize.x / originalSize.x, false);
-        yKnob.SetNonNormalisedValue (curRoomSize.y / originalSize.y, false);
-        zKnob.SetNonNormalisedValue (curRoomSize.z / originalSize.z, false);
+        xKnob.SetNonNormalisedValue(curRoomSize.x / originalSize.x, false);
+        yKnob.SetNonNormalisedValue(curRoomSize.y / originalSize.y, false);
+        zKnob.SetNonNormalisedValue(curRoomSize.z / originalSize.z, false);
     }
 
     public Vector3 GetNormalisedPlayerPosition() { return normalisedPlayerPos; }
     public Vector3 GetNonNormalisedPlayerPosition() { return nonNormalisedPlayerPos; }
 
     // Functions found online to get the bounds of a game object
-    Bounds getRenderBounds (GameObject go)
+    Bounds getRenderBounds(GameObject go)
     {
-        Bounds bounds = new  Bounds(Vector3.zero,Vector3.zero);
+        Bounds bounds = new Bounds(Vector3.zero, Vector3.zero);
         Renderer render = go.GetComponent<Renderer>();
-        if(render!=null){
+        if (render != null)
+        {
             return render.bounds;
         }
         return bounds;
     }
 
-    public Bounds getBounds (GameObject go)
+    public Bounds getBounds(GameObject go)
     {
         Bounds bounds;
         Renderer childRender;
         bounds = getRenderBounds(go);
-        if(bounds.extents.x == 0){
-            bounds = new Bounds(go.transform.position,Vector3.zero);
-            foreach (Transform child in go.transform) {
+        if (bounds.extents.x == 0)
+        {
+            bounds = new Bounds(go.transform.position, Vector3.zero);
+            foreach (Transform child in go.transform)
+            {
                 childRender = child.GetComponent<Renderer>();
-                if (childRender) {
+                if (childRender)
+                {
                     bounds.Encapsulate(childRender.bounds);
-                }else{
+                }
+                else
+                {
                     bounds.Encapsulate(getBounds(child.gameObject));
                 }
             }
